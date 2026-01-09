7f5e93b46e19f90b8811c24f5b738f1a4832795b
Aldair <rd544596@gmail.com>
2026-01-09 18:18:56 -0500

    Exercise 3.9

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 morgan.token('body', (request) => {
     return JSON.stringify(request.body)
 })
 
+app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
